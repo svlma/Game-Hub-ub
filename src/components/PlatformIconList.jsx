8053abab9a33800faded0a0706ed9e627fd891a1
tsubaki <rullo.salma@gmail.com>
2023-07-24 21:53:03 +0100

    Displaying platform icons

---
+++ src/components/PlatformIconList.jsx
@@ -0,0 +1,35 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { HStack, Icon } from "@chakra-ui/react";
+
+const PlatformIconList = ({ platforms }) => {
+  const iconMap = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    nintendo: SiNintendo,
+  };
+  return (
+    <HStack margin={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
