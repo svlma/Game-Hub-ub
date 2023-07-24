98d770c40fda6012a8318e73d08d81808a1afd69
tsubaki <rullo.salma@gmail.com>
2023-07-24 22:11:49 +0100

    Displaying critic score

--- src/components/GameCards.jsx
+++ src/components/GameCards.jsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import React from "react";
 import PlatformIconList from "./PlatformIconList";
+import CriticsScore from "./CriticsScore";
 
 const GameCard = ({ game }) => {
   return (
@@ -8,9 +9,12 @@ const GameCard = ({ game }) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticsScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
