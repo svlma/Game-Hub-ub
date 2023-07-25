e7cad6428a4e4b4202cfd834e3acc801ce4b8e56
tsubaki <rullo.salma@gmail.com>
2023-07-25 19:14:59 +0100

    Display genres

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ function App() {
   return (
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+      templateColumns={{ base: "1fr", lg: "240px 1fr" }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
 
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX="8px">
           <GenreList />
         </GridItem>
       </Show>
--- src/components/GameCardContainer.jsx
+++ src/components/GameCardContainer.jsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 const GameCardContainer = ({ children }) => {
   return (
-    <Box width="250px" borderRadius={10} overflow="hidden">
+    <Box width="100%" borderRadius={10} overflow="hidden">
       {children}
     </Box>
   );
