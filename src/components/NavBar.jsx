11520ad481e3b200e7f7251a23425b0138e62119
tsubaki <rullo.salma@gmail.com>
2023-07-29 21:18:36 +0100

    Update website logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,11 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/cat2.webp";
+import logo from "../assets/cat.webp";
 import ColorModeSwitch from "./colorModeSwitch";
 import SearchInput from "./SearchInput";
 const NavBar = ({ onSearch }) => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Image src={logo} boxSize="60px" borderRadius={11} objectFit="cover" />
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
